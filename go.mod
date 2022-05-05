2d7d29c381e85dc516e0276be733b784727d0a43
Diego Maia <diego.maia@olx.com>
2022-05-05 09:20:08 +0100

    strings

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/diegocmsantos/golang-with-tests
+
+go 1.17
---
+++ hello.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+const (
+	spanish            = "Spanish"
+	french             = "French"
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
+
+func Hello(name, language string) string {
+	if name == "" {
+		name = "world"
+	}
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) string {
+	prefix := ""
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return prefix
+}
+
+func main() {
+	fmt.Println(Hello("world", ""))
+}
