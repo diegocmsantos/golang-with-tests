9f69a64db8bd158c9b2ea19faee385dc34480374
Diego Maia <diego.maia@olx.com>
2022-05-05 09:42:01 +0100

    interation

---
+++ interation/interation_test.go
@@ -0,0 +1,17 @@
+package interation
+
+import "testing"
+
+func TestRepeat(t *testing.T) {
+	got := Repeat("a")
+	want := "aaaaa"
+	if got != want {
+		t.Errorf("want: %s, but got: %s", want, got)
+	}
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a")
+	}
+}
